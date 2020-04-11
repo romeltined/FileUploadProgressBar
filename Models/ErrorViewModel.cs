3ee50d61ef96cfa10d823620b8842307039b23ab
Romelito Tined <rstined@yahoo.com>
2020-04-11 15:30:30 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FileUploadProgressBar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
